8bb38b38d9b0d529cbf6deb08231c970fc406f0b
Klishin Pavel <Klishin.Pavel@infotecs.ru>
2016-05-16 16:21:27 +0300

    Initial commit

---
+++ app/src/androidTest/java/pavel/klishin/vacancytestingapp/ApplicationTest.java
@@ -0,0 +1,18 @@
+package pavel.klishin.vacancytestingapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import dalvik.annotation.TestTargetClass;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+    //dd if=/dev/zero of=/sdcard/file.txt bs=1000000000M count=1
+
+}
